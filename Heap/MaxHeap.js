03e27b359db6ca6b3266878e1625d164b095d391
abhirampjayan <abhiramp08@gmail.com>
2025-06-23 06:17:32 +0530

    created Heap Data Structures

---
+++ Heap/MaxHeap.js
@@ -0,0 +1,39 @@
+class MaxHeap {
+    constructor(values) {
+        this.values = values || [];
+    }
+
+    add(value) {
+        this.values.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.size() - 1;
+        while (index > 0) {
+            const par = Math.floor((index - 1) / 2);
+            if (this.get(par) < this.get(index)) {
+                this.swap(par, index);
+                index = par;
+            } else {
+                break;
+            }
+        }
+
+    }
+
+    size() {
+        return this.values.length;
+    }
+
+    get(index) {
+        return this.values[index];
+    }
+
+    swap(index1, index2) {
+        [this.values[index1], this.values[index2]] = [this.values[index2], this.values[index1]];
+    }
+
+}
+
+export default MaxHeap;
